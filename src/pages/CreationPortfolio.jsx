3cac958b6481abfe89270af39377551c19748dae
Florifourchette <florifourchette@gmail.com>
2023-04-25 16:43:37 +0200

    first version of form ready, need to merge before test

--- src/pages/CreationPortfolio.jsx
+++ src/pages/CreationPortfolio.jsx
@@ -1,21 +1,38 @@
-import React from 'react'
-import { Button, Checkbox, Form } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Button, Form } from 'semantic-ui-react'
 
 const CreationPortfolio = () => {
+const [newPortfolioName, setNewPortfolioName]=useState('')
+const [newPortfolioInitialAmount, setNewPortfolioInitialAmount]=useState('')
+const [newPortfolioUsername, setNewPortfolioUsername]=useState('')
+
+const handleSubmit = () => {axios.post('/api/portfolio_creation/:userId', {
+    initial_amount: newPortfolioInitialAmount,
+    name_of_portfolio: newPortfolioName,
+    friend_username: newPortfolioUsername
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });}
+
+
   return (
     <>
-      <Form>
+      <Form onSubmit={handleSubmit}>
     <Form.Field>
       <label>Portfolio name</label>
-      <input placeholder='Portfolio name' />
+      <input placeholder='Portfolio name' onChange={(e)=>setNewPortfolioName(e.target.value)}/>
     </Form.Field>
     <Form.Field>
       <label>Initial amount</label>
-      <input placeholder='Initial amount' />
+      <input placeholder='Initial amount' onChange={(e)=>setNewPortfolioInitialAmount(e.target.value)}/>
     </Form.Field>
     <Form.Field>
       <label>Friend username</label>
-      <input placeholder='Friend username' />
+      <input placeholder='Friend username' onChange={(e)=>setNewPortfolioUsername(e.target.value)}/>
     </Form.Field>
     <Button type='submit'>Submit</Button>
   </Form>
