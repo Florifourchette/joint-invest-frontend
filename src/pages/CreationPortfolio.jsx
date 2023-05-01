f84ea632bb7c5f5d93304ada0bdb0b49d3db4aa0
Florifourchette <florifourchette@gmail.com>
2023-05-01 13:03:28 +0200

    message page started

--- src/pages/CreationPortfolio.jsx
+++ src/pages/CreationPortfolio.jsx
@@ -1,27 +1,28 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
-import { Button, Form } from "semantic-ui-react";
-import axios from "axios";
-import "../../styles/App.css";
-import useAuth from "../hooks/useAuth";
-import LogIn from "./LogIn";
-import { Message } from "semantic-ui-react";
-import Navbar from "../components/Navbar";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { Button, Form } from 'semantic-ui-react';
+import axios from 'axios';
+import '../../styles/App.css';
+import useAuth from '../hooks/useAuth';
+import LogIn from './LogIn';
+import { Message } from 'semantic-ui-react';
+import Navbar from '../components/Navbar';
 
 const CreationPortfolio = () => {
-  const [newPortfolioName, setNewPortfolioName] = useState("");
-  const [newPortfolioInitialAmount, setNewPortfolioInitialAmount] = useState(0);
-  const [newPortfolioUsername, setNewPortfolioUsername] = useState("");
+  const [newPortfolioName, setNewPortfolioName] = useState('');
+  const [newPortfolioInitialAmount, setNewPortfolioInitialAmount] =
+    useState(0);
+  const [newPortfolioUsername, setNewPortfolioUsername] =
+    useState('');
   const [uppercaseDetected, setUppercaseDetected] = useState(false);
-  const [checkUsername, setCheckUsername] = useState("");
+  const [checkUsername, setCheckUsername] = useState('');
   const { isAuthenticated } = useAuth();
 
   const { userId } = useParams();
 
-
   useEffect(() => {
     axios
-      .get("http://localhost:3000/api/overview")
+      .get('http://localhost:3000/api/overview')
       .then(function (response) {
         console.log(response);
       })
@@ -35,13 +36,15 @@ const CreationPortfolio = () => {
   };
 
   const handleSubmit = () => {
-    
     axios
-      .post(`http://localhost:3000/api/creation_portfolio/${userId}`, {
-        initial_amount: newPortfolioInitialAmount,
-        name_of_portfolio: newPortfolioName,
-        friend_username: newPortfolioUsername,
-      })
+      .post(
+        `http://localhost:3000/api/creation_portfolio/${userId}`,
+        {
+          initial_amount: newPortfolioInitialAmount,
+          name_of_portfolio: newPortfolioName,
+          friend_username: newPortfolioUsername,
+        }
+      )
       .then(function (response) {
         setCheckUsername(response.data);
       })
@@ -51,15 +54,17 @@ const CreationPortfolio = () => {
   };
 
   useEffect(() => {
-    setCheckUsername("");
+    setCheckUsername('');
     setUppercaseDetected(false);
   }, [newPortfolioUsername]);
 
   return isAuthenticated ? (
     <>
       <h1>Add portfolio</h1>
-      <p className="page-description">Start a new portfolio with your friend</p>
-      <Form onSubmit={handleSubmit}>
+      <p className="page-description">
+        Start a new portfolio with your friend
+      </p>
+      <Form onSubmit={handleSubmit} className="creation_form">
         <Form.Field>
           <label>Portfolio name</label>
           <input
@@ -72,7 +77,9 @@ const CreationPortfolio = () => {
           <label>Initial amount</label>
           <input
             placeholder="Initial amount"
-            onChange={(e) => setNewPortfolioInitialAmount(e.target.value)}
+            onChange={(e) =>
+              setNewPortfolioInitialAmount(e.target.value)
+            }
             type="number"
             required
           />
@@ -88,12 +95,12 @@ const CreationPortfolio = () => {
             }
             required
           />
-          {checkUsername === "user not found" ? (
+          {checkUsername === 'user not found' ? (
             <p>User has not been found</p>
           ) : (
             <p></p>
           )}
-          {checkUsername === "identical ids" ? (
+          {checkUsername === 'identical ids' ? (
             <p>You cannot create a portfolio with yourself</p>
           ) : (
             <p></p>
@@ -106,18 +113,20 @@ const CreationPortfolio = () => {
         </Form.Field>
         <Button
           type="submit"
+          className="creation_submitbtn"
           style={{
-            width: "13em",
-            height: "3em",
-            fontSize: "1.5em",
-            backgroundColor: "#074ee8",
-            border: "none",
-            color: "white",
-            borderRadius: "5px",
-            fontWeight: "bold",
-            padding: "0.5rem 1rem",
-            cursor: "pointer",
-            marginTop: "1.5em",
+            width: '9em',
+            height: '2em',
+            fontSize: '1.3em',
+            backgroundColor: '#074ee8',
+            border: 'none',
+            color: 'white',
+            borderRadius: '5px',
+            fontWeight: 'bold',
+            padding: '0.5rem 1rem',
+            cursor: 'pointer',
+            marginTop: '1.5em',
+            marginBottom: '1em',
           }}
         >
           Submit
@@ -128,7 +137,7 @@ const CreationPortfolio = () => {
   ) : (
     <div>
       <div className="d-flex justify-content-center">
-        <Message style={{ color: "red" }}>
+        <Message style={{ color: 'red' }}>
           You are not logged in, please login!
         </Message>
       </div>
