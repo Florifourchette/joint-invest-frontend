819e1e9c7acf433d6976577ab285d8221f0f8cde
Alvaro Perez Pintado <122454645+AlvaroNW@users.noreply.github.com>
2023-05-02 16:31:49 +0200

    search function ready

--- src/components/StockSearch.jsx
+++ src/components/StockSearch.jsx
@@ -5,45 +5,90 @@ export default function StockSearchBar() {
     const [searchQuery, setSearchQuery] = useState("");
     const [searchOptions, setSearchOptions] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [selectedOption, setSelectedOption] = useState("");
 
     useEffect(() => {
         async function fetchSearchOptions() {
             setIsLoading(true);
 
-            const response = await fetch(`/api/search?q=${searchQuery}`);
-            const data = await response.json();
+            const response = await fetch(
+                `https://api.twelvedata.com/symbol_search?symbol=${searchQuery}&show_plan=true`
+            );
+            const responseData = await response.json();
+            const data = responseData.data;
+            console.log(data);
 
-            setSearchOptions(data);
+            // filtering out results not included in the plan
+            const filteredData = data.filter((stock) => {
+                return stock.access.plan === "Basic";
+            });
+
+            const options = filteredData.map((stock, index) => ({
+                key: index,
+                text: `${stock.instrument_name} (${stock.symbol})`,
+                value: stock.symbol,
+            }));
+
+            setSearchOptions(options);
             setIsLoading(false);
         }
-
-        if (searchQuery) {
+        if(searchQuery === '') {
+            setSearchOptions([])
+        }
+        if (searchQuery.length > 1) {
             fetchSearchOptions();
-        } else {
-            setSearchOptions([]);
+            
         }
     }, [searchQuery]);
-
+    console.log('search query',searchQuery);
+    const handleSelect = (option) => {
+        setSelectedOption(option);
+    };
+    function handleClear() {
+        setSelectedOption("");
+    }
+    console.log("selected option", selectedOption);
+    console.log(searchOptions);
     return (
-        <div className="ui search">
+        <div className="ui search transaction-search">
             <div className="ui icon input">
                 <input
                     className="prompt"
                     type="text"
-                    placeholder="Find new stocks"
-                    value={searchQuery}
+                    placeholder={
+                        selectedOption === ""
+                            ? "Find new stocks"
+                            : selectedOption
+                    }
+                    value={selectedOption === "" ? searchQuery : selectedOption}
                     onChange={(event) => setSearchQuery(event.target.value)}
                 />
-                <i className="search icon"></i>
+                {selectedOption !== "" && (
+                    <button><i className="close icon" onClick={handleClear}></i></button>
+                )}
+                {selectedOption === "" && (
+                    <i className="search icon"></i>
+                )}
+                
             </div>
             {isLoading && <Loader active inline />}
-            {!isLoading && searchOptions.length > 0 && (
-                <Dropdown
-                    className="results"
-                    fluid
-                    selection
-                    options={searchOptions}
-                />
+            {!isLoading &&
+                searchOptions.length > 0 &&
+                selectedOption === "" && (
+                    <Dropdown.Menu className="drop-down">
+                        {searchOptions.map((option, index) => (
+                            <Dropdown.Item
+                                key={index}
+                                text={option.text}
+                                value={option.value}
+                                onClick={() => handleSelect(option.text)}
+                                className="dropdown-item"
+                            />
+                        ))}
+                    </Dropdown.Menu>
+                )}
+            {!isLoading && searchOptions.length === 0 && (
+                <p>No results found.</p>
             )}
         </div>
     );
