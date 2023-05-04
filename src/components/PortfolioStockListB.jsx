29875073f46d64f1f6743d550388de23121ec691
Bao Nguyen <Minzekatzebhn@gmail.com>
2023-05-04 20:42:22 +0200

    portfolio-styling

--- src/components/PortfolioStockListB.jsx
+++ src/components/PortfolioStockListB.jsx
@@ -14,63 +14,88 @@ const StockListB = ({ item, externalAPIstocks, stockData, sharePrice }) => {
 
   return (
     <>
-      <List relaxed="very">
-        <List.Item className="d-flex">
-          <Image
-            avatar
-            src={logo ? `/company_logos/${logo}` : `/company_logos/NO_LOGO.png`}
-          />
-          <List.Content className="container d-flex w-100">
-            <div>
-              <List.Header className="d-flex flex-row text-nowrap " as="a">
-                {item.company_id} ({item.current_number_of_stocks})
-              </List.Header>
-              <List.Description style={{ fontWeight: "800", color: "grey" }}>
-                {parseFloat(sharePrice[item.company_id]).toFixed(2)}
-              </List.Description>
+      <div className="one-stock">
+        <List relaxed="very">
+          <List.Item className="d-flex">
+            <div className="stock-shade">
+              <Image
+                style={{ height: "40px", width: "40px" }}
+                avatar
+                src={
+                  logo ? `/company_logos/${logo}` : `/company_logos/NO_LOGO.png`
+                }
+              />
+              <List.Content className="container d-flex w-100">
+                <div>
+                  <List.Header
+                    className="d-flex flex-row text-nowrap "
+                    style={{ color: "#31231E", fontSize: "1.2em" }}
+                  >
+                    {item.company_id} ({item.current_number_of_stocks})
+                  </List.Header>
+                  <List.Description
+                    style={{
+                      fontWeight: "800",
+                      color: "#84714F",
+                      fontSize: "1em",
+                      marginTop: "0.1rem",
+                    }}
+                  >
+                    {parseFloat(sharePrice[item.company_id]).toFixed(2)}
+                  </List.Description>
+                </div>
+                <div className="d-flex justify-content-end w-100 align-items-center">
+                  {parseFloat(sharePrice[item.company_id]) -
+                    item.average_price_buy >
+                  0 ? (
+                    <List.Description
+                      style={{
+                        fontWeight: "600",
+                        color: "#698D1B",
+                        fontSize: "1.2em",
+                      }}
+                    >
+                      +
+                      {(
+                        parseFloat(sharePrice[item.company_id]) /
+                        item.average_price_buy
+                      ).toFixed(2)}
+                      % / +
+                      {(
+                        parseFloat(sharePrice[item.company_id]) *
+                          parseInt(item.current_number_of_stocks) -
+                        item.average_price_buy *
+                          parseInt(item.current_number_of_stocks)
+                      ).toFixed(2)}
+                      $
+                    </List.Description>
+                  ) : (
+                    <List.Description
+                      style={{
+                        fontWeight: "600",
+                        color: "#922727",
+                        fontSize: "1.2em",
+                      }}
+                    >
+                      {parseFloat(
+                        (sharePrice[item.company_id] / item.average_price_buy) *
+                          100 -
+                          100
+                      ).toFixed(2)}
+                      % /
+                      {parseFloat(
+                        (sharePrice[item.company_id] - item.average_price_buy) *
+                          parseInt(item.current_number_of_stocks)
+                      ).toFixed(2)}
+                      $
+                    </List.Description>
+                  )}
+                </div>
+              </List.Content>
             </div>
-            <div className="d-flex justify-content-end w-100 align-items-center">
-              {parseFloat(sharePrice[item.company_id]) -
-                item.average_price_buy >
-              0 ? (
-                <List.Description
-                  style={{ fontWeight: "600", color: "#7AD982" }}
-                >
-                  +
-                  {(
-                    parseFloat(sharePrice[item.company_id]) /
-                    item.average_price_buy
-                  ).toFixed(2)}
-                  % / +
-                  {(
-                    parseFloat(sharePrice[item.company_id]) *
-                      parseInt(item.current_number_of_stocks) -
-                    item.average_price_buy *
-                      parseInt(item.current_number_of_stocks)
-                  ).toFixed(2)}
-                  $
-                </List.Description>
-              ) : (
-                <List.Description
-                  style={{ fontWeight: "600", color: "#DD7060" }}
-                >
-                  {parseFloat(
-                    (sharePrice[item.company_id] / item.average_price_buy) *
-                      100 -
-                      100
-                  ).toFixed(2)}
-                  % /
-                  {parseFloat(
-                    (sharePrice[item.company_id] - item.average_price_buy) *
-                      parseInt(item.current_number_of_stocks)
-                  ).toFixed(2)}
-                  $
-                </List.Description>
-              )}
-            </div>
-          </List.Content>
-        </List.Item>
-      </List>
+          </List.Item>
+        </List>
+      </div>
     </>
   );
 };
