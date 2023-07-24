1f36e569f86d26a7a9e2cf30c6e2d8f073fd380c
Floriane Michelat <122460329+Florifourchette@users.noreply.github.com>
2023-07-24 16:15:18 +0200

    Update MessagesAPIcalls.jsx

--- utils/MessagesAPIcalls.jsx
+++ utils/MessagesAPIcalls.jsx
@@ -1,5 +1,5 @@
 export function getMessages(userId) {
-  return fetch(`http://localhost:3000/api/messages/${userId}`, {
+  return fetch(`https://joint-invest-back-end.onrender.com/api/messages/${userId}`, {
     method: 'GET',
   }).then((response) => response.json());
 }
